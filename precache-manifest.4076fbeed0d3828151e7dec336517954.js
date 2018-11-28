262965c855d17802c4fcb25dea2c2937d159f94a
fang-tanbamrung <tanbamrung_w@hotmail.com>
2018-11-28 17:22:43 +0700

    Updates

---
+++ precache-manifest.4076fbeed0d3828151e7dec336517954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "35dd0f921ddea73180d7",
+    "url": "/robofriends/static/js/main.35dd0f92.chunk.js"
+  },
+  {
+    "revision": "b02756de579d16e17895",
+    "url": "/robofriends/static/js/1.b02756de.chunk.js"
+  },
+  {
+    "revision": "35dd0f921ddea73180d7",
+    "url": "/robofriends/static/css/main.c1684d7c.chunk.css"
+  },
+  {
+    "revision": "b02756de579d16e17895",
+    "url": "/robofriends/static/css/1.cd199f82.chunk.css"
+  },
+  {
+    "revision": "c7949470a8bc5afbbdb2de20b0cd008a",
+    "url": "/robofriends/index.html"
+  }
+];
